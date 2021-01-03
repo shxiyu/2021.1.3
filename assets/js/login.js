f97852a159b50ef2e222aaed4e12a431c0e3f24a
xiyu <xiyu@xj.com>
2021-01-03 11:46:09 +0800

    完成表单验证功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,22 @@ $(function () {
         $('.login-box').show();
         $('.reg-box').hide();
     })
+
+    //从 layui 中获取 form 对象
+    let form = layui.form;
+    //通过 form.verify() 函数自定义校验规则
+    form.verify({
+        //自定义了 pwd 校验规则
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        //校验两次密码是否一致的规则
+        repwd: function (value) {
+            let pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致';
+            }
+        }
+    })
 })
