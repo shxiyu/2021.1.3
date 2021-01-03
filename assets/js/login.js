eb3002448cb5d12d1ee96e36f8ef771e6f85dccf
xiyu <xiyu@xj.com>
2021-01-03 16:00:31 +0800

    完成了注册和登录页面的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,8 @@ $(function () {
 
     //从 layui 中获取 form 对象
     let form = layui.form;
+    //
+    let layer = layui.layer;
     //通过 form.verify() 函数自定义校验规则
     form.verify({
         //自定义了 pwd 校验规则
@@ -27,5 +29,45 @@ $(function () {
                 return '两次密码不一致';
             }
         }
+    })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 阻止默认的提交行为
+        e.preventDefault();
+        //发起Ajax的POST请求
+        let data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        };
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('注册成功，请登录');
+            //模拟点击行为
+            $('#link_login').click();
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').on("submit", function (e) {
+        //阻止默认提交行为
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败');
+                }
+                layer.msg('登录成功');
+                //将登录获取到的token值保存到localStorage
+                localStorage.setItem('token', res.token);
+                //跳转到后台主页
+                location.href = '/index.html';
+            }
+        })
     })
 })
