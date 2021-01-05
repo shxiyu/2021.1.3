fe56f06f1547531465316ef5858b79511cba8bac
xiyu <xiyu@xj.com>
2021-01-05 11:20:49 +0800

    首页基本功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,28 @@
+/*
+ * @Author: XiYu
+ * @Date: 2021-01-03 16:09:03
+ * @LastEditors: XiYu
+ * @LastEditTime: 2021-01-05 11:18:49
+ * @Description: ...
+ */
 $.ajaxPrefilter(function (options) {
-    console.log(options.url);
-    options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+
+    //统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载complete回调函数
+    //不论成功还是失败，都会调用complete回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
+
